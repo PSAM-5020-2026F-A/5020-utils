7e0fec5dea42b63123b00e082990ab25c2f5f0a4
Thiago Hersan <thiago.hersan+github@gmail.com>
2026-08-06 17:05:24 -0400

    initial commit

---
+++ src/text_utils.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def get_nonzero_scores(vectors, sorted_idxs, n_words):
+  # iterate over vectors and list of indices
+  # index each vector by sorted indexes
+  # then slice to keep non-zero values
+  # then slice to keep n_words
+  nz_scores = [row[idx][:len(row.nonzero()[0])][:n_words] for row,idx in zip(vectors, sorted_idxs)]
+  return np.array(nz_scores, dtype=object)
+
+def get_top_words(vectors, vocab, *, n_words=None, return_scores=False):
+  is_row = vectors.shape[0] == 1
+
+  if hasattr(vectors, "toarray"):
+    vectors = vectors.toarray()
+
+  sidxs = (-vectors).argsort()
+  nz_scores = get_nonzero_scores(vectors, sidxs, n_words)
+  # similar to scores... get sorted list of words by sorted indices
+  # then slice up to length of corresponding score list
+  nz_words = np.array([vocab[idx][:len(nzs)] for nzs,idx in zip(nz_scores, sidxs)], dtype=object)
+
+  if is_row:
+    nz_scores = nz_scores.squeeze()
+    nz_words = nz_words.squeeze()
+
+  if return_scores:
+    return nz_words.tolist(), nz_scores.tolist()
+  else:
+    return nz_words.tolist()
